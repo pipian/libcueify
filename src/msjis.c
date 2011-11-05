bc3d7081b5659e7f7ef37d54928fcf93067526a6
Ian Jacobi <pipian@pipian.com>
2011-11-05 01:40:35 -0400

    Properly increment the string pointer after reading one-byte character when converting from UTF-8 in the MS-JIS encoder as well

--- src/msjis.c
+++ src/msjis.c
@@ -120,10 +120,11 @@ size_t msjis_byte_count(char *utf8) {
 	character = 0;
 	if (*bp <= 0x7F) {
 	    /* 1-byte codepoint */
-	    character = *bp;
+	    character = *bp++;
 	} else if (*bp <= 0xBF) {
 	    /* Invalid codepoint */
 	    character = '?';
+	    bp++;
 	} else if (*bp <= 0xDF) {
 	    /* 2-byte codepoint */
 	    character  = (*bp++ & 0x1F) << 6;
@@ -224,10 +225,11 @@ uint8_t *utf8_to_msjis(char *utf8, size_t *size) {
 	character = 0;
 	if (*bp <= 0x7F) {
 	    /* 1-byte codepoint */
-	    character = *bp;
+	    character = *bp++;
 	} else if (*bp <= 0xBF) {
 	    /* Invalid codepoint */
 	    character = '?';
+	    bp++;
 	} else if (*bp <= 0xDF) {
 	    /* 2-byte codepoint */
 	    character  = (*bp++ & 0x1F) << 6;
